db431028b4bf484c09b11693610d41c5e76113b4
rmorleysmith <richwins@hotmail.co.uk>
2023-05-03 23:19:17 +0100

    Get multiple threads going

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,69 @@
-use std::collections::HashSet;
+use std::{collections::HashSet, sync::{Arc, Mutex}, thread};
 use scraper::{Html, Selector};
 use url::Url;
+use crossbeam_channel::unbounded;
 
 fn main() {
     crawl_from_seed("https://www.bbc.co.uk");
 }
 
 fn crawl_from_seed(seed: &str) {
-    // Add the seed URL to the crawl queue
-    let mut crawl_queue = HashSet::new();
-    crawl_queue.insert(Url::parse(seed).unwrap());
-
     // Keep track of crawled URLs
-    let mut crawled_urls = HashSet::new();
+    let crawled_urls = Arc::new(Mutex::new(HashSet::new()));
 
-    while !crawl_queue.is_empty() {
-        // Get the next URL to crawl
-        let url_to_crawl = crawl_queue.iter().next().unwrap().clone();
+    // Create a channel so that threads can communicate with each other
+    let (sender, receiver) = unbounded();
 
-        // Remove the URL from the queue
-        crawl_queue.remove(&url_to_crawl);
+    // Use 16 threads
+    for i in 0..16 {
+        let sender = sender.clone();
+        let receiver = receiver.clone();
+        let crawled_urls = crawled_urls.clone();
 
-        // Check if we crawled this URL yet
-        if crawled_urls.contains(&url_to_crawl) {
-            continue;
-        }
+        thread::spawn(move || loop {
+            let url: Url = match receiver.recv() {
+                Ok(url) => url,
+                Err(_) => break,
+            };
 
-        // Fetch the URL and print the response
-        let response = reqwest::blocking::get(url_to_crawl.as_str()).unwrap();
-        let body = response.text().unwrap();
-        // Add any links found to the set of URLs to crawl
-        let new_urls = extract_links(&body);
-        for new_url in new_urls {
-            if !crawled_urls.contains(&new_url) {
-                crawl_queue.insert(new_url);
+            println!("Thread {} received URL {}", i, url);
+
+            // Fetch the URL
+            let response = reqwest::blocking::get(url.as_str()).unwrap();
+            let body = response.text().unwrap();
+
+            // Add any links found to the set of URLs to crawl
+            let new_urls = extract_links(&body);
+            
+            // Get crawled URLs with a lock 
+            let mut crawled_urls = crawled_urls.lock().unwrap();
+
+            // Insert the URL we just crawled
+            crawled_urls.insert(url.clone());
+
+            for new_url in new_urls {
+                if !crawled_urls.contains(&new_url) {
+                    match sender.send(new_url) {
+                        Ok(_) => (),
+                        Err(_) => println!("Error sending new URL to channel"),
+                    }
+                }
             }
-        }
-        
-        // Print out the URL we just crawled
-        println!("Crawled: {}", &url_to_crawl);
-        
-        // Add this URL to the crawled list
-        crawled_urls.insert(url_to_crawl);
+
+            drop(crawled_urls);
+
+             // Print out the URL we just crawled
+            println!("Crawled: {}", url);
+        });
     }
+
+    match Url::parse(seed) {
+        Ok(url) => { sender.send(url).unwrap() },
+        Err(_) => (),
+    }
+
+    // Keep going until we stop
+    loop {}
 }
 
 fn extract_links(html: &str) -> HashSet<Url> {
